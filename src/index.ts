4d707f40dd7d82504c30eb93e13655463bd9a413
fede-rodes <federodes@gmail.com>
2023-12-14 11:40:05 +1300

    adding fetcher config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 /// <reference path="../node_modules/@vechain/connex-types/index.d.ts" />
 
+export type Address = `0x${string}`
+
 export type ChainId = 100009 | 100010 | 100011;
 // ^ 100009 = production, 100010 = staging, 100011 = development
+
+export type DexName = "verocket" | "vexchange"
+
+export type Dex = {
+  /** DEX name. */
+  name: DexName;
+  /** UniV2 router contract address. */
+  routerV2: Address;
+  /** VVET-VTHO pair contract address. */
+  pairVVET_VTHO: Address;
+}
 
 /**
  * JavaScript CAIP-2 representation object.
@@ -30,13 +43,23 @@ export type ChainData = {
     standard: string;
   }[];
   /** VTHO contract address. */
-  vtho: string;
+  vtho: Address;
+  /** VVET contract address. */
+  vvet: Address;
+  /** List of supported DEXs. */
+  dexs: Dex[];
   /** Trader contract address. */
-  trader: string;
+  trader: Address;
+  /** Register events endpoint. */
+  registerEventsEndpoint: string;
+  /** Endpoint to fetch latest block number. */
+  getHeadEndpoint: string;
   getAccountSwapsEndpoint: string;
   getAccountStatsEndpoint: string;
   getTradeForecastEndpoint: string;
 };
+
+// TODO: extend development from staging
 
 /**
  * @link https://github.com/ethereum-lists/chains
@@ -69,8 +92,23 @@ export const CHAINS: Record<ChainId, ChainData> = {
         standard: "none",
       },
     ],
-    vtho: "0x0000000000000000000000000000456E65726779",
-    trader: "0x0000000000000000000000000000000000000000", // TODO
+         vtho: "0x0000000000000000000000000000456E65726779", // token0
+     vvet: "0x45429a2255e7248e57fce99e7239aed3f84b7a53", // token1
+     dexs: [
+       {
+         name: "verocket",
+         routerV2: "0x576da7124c7bb65a692d95848276367e5a844d95",
+         pairVVET_VTHO: "0x29a996b0ebb7a77023d091c9f2ca34646bea6ede",
+       },
+       {
+         name: "vexchange",
+         routerV2: "0x6c0a6e1d922e0e63901301573370b932ae20dadb",
+         pairVVET_VTHO: "0x0000000000000000000000000000000000000000", // TODO
+       },
+     ],
+     trader: "0x0000000000000000000000000000000000000000", // TODO
+     getHeadEndpoint: "https://",
+     registerEventsEndpoint: "https://",
     getAccountSwapsEndpoint: "https://",
     getAccountStatsEndpoint: "https://",
     getTradeForecastEndpoint: "https://",
@@ -97,8 +135,23 @@ export const CHAINS: Record<ChainId, ChainData> = {
         standard: "none",
       },
     ],
-    vtho: "0x0000000000000000000000000000456E65726779",
-    trader: "0x0317B19b8b94aE1D5Bfb4727b9064fe8118aA305",
+         vtho: "0x0000000000000000000000000000456E65726779", // token0
+     vvet: "0x86fb5343bbecffc86185c023a2a6ccc76fc0afd8", // token1
+     dexs: [
+       {
+         name: "verocket",
+         routerV2: "0x91e42759290239a62ac757cf85bb5b74ace57927",
+         pairVVET_VTHO: "0x1e5e9a6540b15a3efa8d4e8fadb82cc8e0e167ca",
+       },
+       {
+         name: "vexchange",
+         routerV2: "0x01d6b50b31c18d7f81ede43935cadf79901b0ea0",
+         pairVVET_VTHO: "0x0000000000000000000000000000000000000000",
+       },
+     ],
+     trader: "0x0317B19b8b94aE1D5Bfb4727b9064fe8118aA305",
+     getHeadEndpoint: "https://gethead-3co32ksh6a-uc.a.run.app",
+     registerEventsEndpoint: "https://registerevents-3co32ksh6a-uc.a.run.app",
     getAccountSwapsEndpoint: "https://getaccountswaps-3co32ksh6a-uc.a.run.app",
     getAccountStatsEndpoint: "https://getaccountstats-3co32ksh6a-uc.a.run.app",
     getTradeForecastEndpoint:
@@ -126,8 +179,23 @@ export const CHAINS: Record<ChainId, ChainData> = {
         standard: "none",
       },
     ],
-    vtho: "0x0000000000000000000000000000456E65726779",
-    trader: "0x0317B19b8b94aE1D5Bfb4727b9064fe8118aA305",
+         vtho: "0x0000000000000000000000000000456E65726779", // token0
+     vvet: "0x86fb5343bbecffc86185c023a2a6ccc76fc0afd8", // token1
+     dexs: [
+       {
+         name: "verocket",
+         routerV2: "0x91e42759290239a62ac757cf85bb5b74ace57927",
+         pairVVET_VTHO: "0x1e5e9a6540b15a3efa8d4e8fadb82cc8e0e167ca",
+       },
+       {
+         name: "vexchange",
+         routerV2: "0x01d6b50b31c18d7f81ede43935cadf79901b0ea0",
+         pairVVET_VTHO: "0x0000000000000000000000000000000000000000",
+       },
+     ],
+     trader: "0x0317B19b8b94aE1D5Bfb4727b9064fe8118aA305",
+     getHeadEndpoint: "http://127.0.0.1:5001/vefarmdev/us-central1/gethead",
+     registerEventsEndpoint: "http://127.0.0.1:5001/vefarmdev/us-central1/registerevents",
     getAccountSwapsEndpoint:
       "http://127.0.0.1:5001/vefarmdev/us-central1/getaccountswaps",
     getAccountStatsEndpoint:
